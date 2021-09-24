a625a34f2a6beec893c8deb15cbe6cd464063b51
Kevin King <kevinturingking@gmail.com>
2021-09-24 14:25:51 -0600

    complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,23 @@
+var movie = "Fury"
+console.log(movie);
+
+var isEntertaining = false
+console.log(isEntertaining);
+
+movie = "Hitch"
+console.log(movie);
+
+var num4 = 13
+var num6 = 7
+var product = (num4 * num6)
+console.log(product)
+
+var favoriteFoods = ["Burger", "Pizza", "Tacos"]
+console.log(favoriteFoods);
+
+var favoriteFoods = ["Burger", "Pizza", "Tacos"];
+favoriteFoods.shift();
+console.log(favoriteFoods);
+
+movie = "Hitch"
+console.log(movie.length)
