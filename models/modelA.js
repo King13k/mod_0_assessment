2a43617ceecbcee12c9272ef62309c214cb54bd8
Kevin King <kevinturingking@gmail.com>
2021-09-24 14:37:50 -0600

    add modelA.js

---
+++ models/modelA.js
@@ -0,0 +1,12 @@
+var Dinosaur = {
+  name: "Trex",
+  placeOfBirth: "North America",
+  weightInlbs: 10000,
+  heightInfeet: 15,
+  goodSwimmer: true,
+  eatsPlants: false,
+  relatives: ["Chickens", "Ostrich"],
+  prey: ["Edmontosaurus", "Triceratops"]
+
+};
+console.log(Dinosaur)
