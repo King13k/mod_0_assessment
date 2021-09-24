e3b2c9291da8e9eea4313454aa8c73630982cd01
Kevin King <kevinturingking@gmail.com>
2021-09-24 14:44:59 -0600

    add modelB.js

---
+++ models/modelB.js
@@ -0,0 +1,12 @@
+var Developer = {
+  name: "Kevin",
+  codingProgram: "Front-end",
+  age: 27,
+  heightInfeet: 6,
+  codedBefore: false,
+  learningTocode: true,
+  hobbies: ["Fitness", "Walking dogs", "Eating"],
+  futureGoals: ["Coding Career", "Father", "Travel"]
+
+};
+console.log(Developer)
